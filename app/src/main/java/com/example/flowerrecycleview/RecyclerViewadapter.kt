5530d545dc9013656d3466b6fe51d9ae6c008c74
fatinrahma26 <fatinrahma137@gmail.com>
2019-10-20 14:13:03 +0700

    first commit

---
+++ app/src/main/java/com/example/flowerrecycleview/RecyclerViewadapter.kt
@@ -0,0 +1,14 @@
+package com.example.flowerrecycleview
+
+import android.content.ClipData
+import android.content.Context
+import android.view.ViewParent
+
+class RecyclerViewAdapter(private val context: Context, private  val items: List<ClipData.Item>):
+        RecyclerViewAdapter.Adapter<RecyclerViewAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(
+        z
+    )
+}
